76f2f5a2d85c4d172b27ca6306a96e5673db2f6e
D14125513 <D14125513@mydit.ie>
2017-04-21 23:28:01 +0100

    Update MyDBHandler.java

--- MyDBHandler.java
+++ MyDBHandler.java
@@ -7,3 +7,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "myshopping.db";
+    public static final String TABLE_GROCERY = "grocery";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+
+
+    //We need to pass database information along to superclass
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
