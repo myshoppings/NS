b5a6a52313915b26b4cae1a6cc495777ad42b1c7
d14127025 <d14127025@mydit.ie>
2017-04-21 23:19:38 +0100

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sultan.ShoppingList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.sultan.myshoppings.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
