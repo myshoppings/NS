8a808364b2fd0f6c648693b429c30c2af1aa74aa
D14125513 <D14125513@mydit.ie>
2017-04-21 23:22:01 +0100

    Update Grocery.java

--- Grocery.java
+++ Grocery.java
@@ -7,4 +7,16 @@ public class Grocery{
   private String _groceryname;
   
   
+ public Grocery() {
+ }
+  
+  public Grocery(String groceryname) { this. _groceryname = groceryname;}
+  
+  public int get_id() {return _id;}
+  
+  public void set_id(int _id) {this._id =_id;}
+  
+  public String get_groceryname() { return this._groceryname;}
+  public void set_groceryname(String _groceryname) { this._groceryname = _groceryname;}
+  
 }
